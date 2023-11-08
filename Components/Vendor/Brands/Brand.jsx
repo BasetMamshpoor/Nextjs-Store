707c6b4d1ad2ed03aebeed436414c3d30e368d2d
Baset mamshpoor <baset.jaldian@gmail.com>
2023-11-08 15:52:57 +0330

    admin brand is completed

--- Components/Vendor/Brands/Brand.jsx
+++ Components/Vendor/Brands/Brand.jsx
@@ -2,30 +2,40 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import style from './Brand.module.css'
 
-const Brand = ({ value, id, setIsOpen }) => {
+const Brand = ({ SwalStyled, reload, value, id, setIsOpen }) => {
     const [name, setName] = useState()
 
     const handleChange = (e) => setName(e.target.value)
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault()
         if (!value) {
             await axios.post('/admin/brands', { name })
-                .then(() => setIsOpen(false))
-                .catch(err => console.log(err))
+                .then(() => {
+                    SwalStyled.fire('.ثبت شد', '.برند مورد نظر با موفقیت ثبت شد', 'success')
+                    reload(Math.random())
+                    setIsOpen(false)
+                })
+                .catch(err => SwalStyled.fire('.ثبت نشد', '.مشکلی در هنگام ثبت به وجود آمد', 'error'))
         } else {
             await axios.put(`/admin/brands/${id}`, { name })
-                .then(() => setIsOpen(false))
-                .catch(err => console.log(err))
+                .then(() => {
+                    SwalStyled.fire('.ویرایش شد', '.برند مورد نظر با موفقیت ویرایش شد', 'success')
+                    reload(Math.random())
+                    setIsOpen(false)
+                })
+                .catch(err => SwalStyled.fire('.ثبت نشد', '.مشکلی در هنگام ویرایش به وجود آمد', 'error'))
         }
     }
 
     return (
         <>
             <div className={style.brand}>
-                <div className={style.form}>
-                    <input value={name} name={name} onChange={handleChange} type="text" defaultValue={!!value ? value : null} className={style.input} />
-                    <button type='submit' onClick={handleSubmit} className={style.button}>{!!value ? 'تغییر' : 'ثبت'}</button>
-                </div>
+                <form className={style.form} onSubmit={handleSubmit}>
+                    <h3>{!!value ? 'ویرایش برند' : 'برند جدید'}</h3>
+                    <input required value={name} name={name} onChange={handleChange} type="text" defaultValue={!!value ? value : null} className={style.input} />
+                    <button className={style.button}>{!!value ? 'تغییر' : 'ثبت'}</button>
+                </form>
             </div>
         </>
     );
--- Components/Vendor/Brands/Brands.jsx
+++ Components/Vendor/Brands/Brands.jsx
@@ -2,38 +2,42 @@ import axios from 'axios';
 import createModal from 'Components/Modal';
 import Pagination from 'Components/Pagination/Pagination';
 import useRequest from 'hooks/useRequest';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { BsPencilSquare, BsPlus, BsTrash } from 'react-icons/bs';
 import Brand from './Brand';
 import style from './Brands.module.css'
+import { Functions } from 'providers/FunctionsProvider';
 
 const Brands = () => {
-    const [brands, setBrands] = useRequest('/admin/brands')
+    const [currentpage, setCurrentpage] = useState(1)
+    const [brands, setBrands, reload, paginations] = useRequest(`/admin/brands?page=${currentpage}`)
+    const { SwalStyled } = useContext(Functions)
 
     const handleDelete = (id) => {
         axios.delete(`/admin/brands/${id}`)
             .then(() => setBrands(prev => {
+                SwalStyled.fire('.حذف شد', '.برند مورد نظر با موفقیت حذف شد', 'success')
                 const newBrands = prev.filter(b => b.id !== id)
                 return newBrands
             }))
     }
     return (
         <>
-            <div className={style.brands}>
+            {!!brands && <div className={style.brands}>
                 <div className={style.container}>
                     <div className={style.header}>
                         <div className={style.name}>برند ها</div>
-                        <div className={style.newbrand} onClick={() => createModal(<Brand />)}><BsPlus /></div>
+                        <div className={style.newbrand} onClick={() => createModal(<Brand SwalStyled={SwalStyled} reload={reload} />)}><BsPlus /></div>
                     </div>
                     <div className={style.content}>
                         <ul className={style.brandlist}>
-                            {!!brands && brands.map(e => {
+                            {brands.map(e => {
                                 return (
                                     <li className={style.brand} key={e.id}>
                                         <p className={style.brandname}>{e.name}</p>
                                         {/* <p className={style.brandcreate}>{(new Date(e.created_at).toLocaleDateString('fa-IR', { year: 'numeric', month: 'long', day: 'numeric' }))}</p> */}
                                         <div className={style.options}>
-                                            <span className={style.edit} onClick={() => createModal(<Brand value={e.name} id={e.id} />)}><BsPencilSquare /></span>
+                                            <span className={style.edit} onClick={() => createModal(<Brand SwalStyled={SwalStyled} reload={reload} value={e.name} id={e.id} />)}><BsPencilSquare /></span>
                                             <span className={style.delete} onClick={() => handleDelete(e.id)}><BsTrash /></span>
                                         </div>
                                     </li>
@@ -42,8 +46,8 @@ const Brands = () => {
                         </ul>
                     </div>
                 </div>
-                <Pagination boxShadow={false} />
-            </div>
+                <Pagination currentPage={currentpage} setCurrentPage={setCurrentpage} dataLength={paginations.meta.total} itemsPerPage={paginations.meta.per_page} boxShadow={false} />
+            </div>}
         </>
     );
 };
